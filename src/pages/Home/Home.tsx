052a3190579552ccad3c5c71af04d52a40c57464
qeryz <czkmaz@gmail.com>
2025-06-07 11:42:48 -0700

    chore: remove app.css

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,18 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../../App.css'
+import { useState } from "react";
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+    <div>
+      <h1 className="flex justify-center">Testing Typography</h1>
+      <div className="flex justify-center">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
