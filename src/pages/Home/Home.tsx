bc34d81a5f84b65e642bb0d78afb48e326d9e75c
qeryz <czkmaz@gmail.com>
2025-06-08 21:49:31 -0700

    chore: testing style changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
-
 function Home() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1 className="flex justify-center">Testing Typography</h1>
-      <div className="flex justify-center">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="flex-col justify-center items-center p-50">
+      <h1 className="text-[48px] font-semibold text-titleMain mb-5">A fully integrated suite of financial and payments products
+
+      </h1>
+      <div className="text-textMain text-[18px]">
+        <p>
+          Reduce costs, grow revenue, and run your business more efficiently on a fully integrated, AI-powered platform. Use Stripe to handle all of your payments-related needs, manage revenue operations, and launch (or invent) new business models.
+        </p>
       </div>
     </div>
   );
