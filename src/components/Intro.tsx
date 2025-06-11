161a3a00a62cfd2327b409c600e373b3aa9a1fca
qeryz <czkmaz@gmail.com>
2025-06-11 14:26:37 -0700

    feat: add intro component

---
+++ src/components/Intro.tsx
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+
+export const Intro = () => {
+  return (
+    <>
+      <motion.h1
+        className="text-[48px] font-semibold text-titleMain mb-5"
+        initial={{ opacity: 0, y: -10, filter: "blur(8px)" }}
+        animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+        transition={{ duration: 1.4 }}
+      >
+        Meet Your Agent from Another Galaxy
+      </motion.h1>
+      <motion.div
+        className="text-textMain text-[18px]"
+        initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+        animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+        transition={{ duration: 1.4, delay: 0.3 }}
+      >
+        <p>
+          A highly intelligent extra-terrestrial being with a vast knowledge of
+          the universe who has traveled across galaxies to share wisdom and
+          insights with those seeking enlightenment. This guru possesses a
+          unique perspective
+        </p>
+      </motion.div>
+    </>
+  );
+};
