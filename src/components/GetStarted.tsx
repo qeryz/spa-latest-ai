6873633fc27a2af862bf7e62d02383e2112d1a77
qeryz <czkmaz@gmail.com>
2025-06-11 14:26:49 -0700

    feat: add get started button

---
+++ src/components/GetStarted.tsx
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+
+export const GetStarted = ({
+  handleClick,
+}: {
+  handleClick: (arg0: boolean) => void;
+}) => {
+  return (
+    <motion.button
+      initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+      animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+      transition={{ duration: 1.4, delay: 1.2 }}
+      className="bg-slate-700 text-white px-6 py-2 rounded-xl mt-4 cursor-pointer font-semibold"
+      onClick={() => handleClick(false)}
+    >
+      Get Started
+    </motion.button>
+  );
+};
